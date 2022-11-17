2f13d12cb92cdb34ca33e84269ee7c31d5d931b5
C. Lewis <ctj.lewis@icloud.com>
2022-11-17 13:06:04 -0600

    chore: point finger for OpenAI API errors

--- packages/upg/src/lib/utils/formatting.ts
+++ packages/upg/src/lib/utils/formatting.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { chalk } from "../globs/shared";
-import { log } from "./log";
+import { log, error } from "./log";
 
 /* eslint-disable no-console */
 export const withFormatting = (fn: (...args: any[]) => any) => {
   return async (...args: any[]) => {
     try {
       return await fn(...args);
-    } catch (error) {
+    } catch (e) {
       const errorMessage =
-        error instanceof Error
-          ? error.message
-          : JSON.stringify(error);
+        e instanceof Error
+          ? e.message
+          : JSON.stringify(e);
+
+      if (errorMessage === "Request failed with status code 503") {
+        error("Issue with OpenAI API. Please see the status page at:", "https://status.openai.com/");
+      }
 
       log(`${chalk.red("ERROR:")} ${errorMessage}`);
 
