46acbb3d0bfb8b3b28b4062035f40448160d2332
C. Lewis <ctj.lewis@icloud.com>
2022-12-03 04:51:38 -0600

    chore: update tests

--- packages/upg/test/commands.test.ts
+++ packages/upg/test/commands.test.ts
@@ -13,7 +13,7 @@ test.serial("zsh", async (t) => {
   if (process.env.CI) return t.pass();
 
   const { stdout } = await shell.run(
-    "upg -n zsh \"save the last year of stock ticker data for Google, Amazon, and Apple to CSVs from Yahoo Finance\""
+    "./dist/bin.js -n zsh \"print the first 30 Mersenne primes\""
   );
 
   t.snapshot(stdout);
@@ -23,7 +23,7 @@ test.serial("python", async (t) => {
   if (process.env.CI) return t.pass();
 
   const { stdout } = await shell.run(
-    "upg -n python \"load the ticker history from tickers.csv, calculate the cumulative returns of tickers by price_close over time, and plot using ggplot and pandas\""
+    "./dist/bin.js -n python \"load the ticker history from tickers.csv, calculate the cumulative returns of tickers by price_close over time, and plot using ggplot and pandas\""
   );
 
   t.snapshot(stdout);
@@ -33,7 +33,7 @@ test.serial("typescript", async (t) => {
   if (process.env.CI) return t.pass();
 
   const { stdout } = await shell.run(
-    "upg -n typescript \"define Y combinator function Y(f) and demo using console.log\""
+    "./dist/bin.js -n typescript \"define Y combinator function Y(f) and demo using console.log\""
   );
 
   t.snapshot(stdout);
@@ -43,7 +43,17 @@ test.serial("mandelbrot", async (t) => {
   if (process.env.CI) return t.pass();
 
   const { stdout } = await shell.run(
-    "upg -n python \"plot the Mandelbrot set\""
+    "./dist/bin.js -n python \"plot the Mandelbrot set\""
+  );
+
+  t.snapshot(stdout);
+});
+
+test.serial("Next.js page", async (t) => {
+  if (process.env.CI) return t.pass();
+
+  const { stdout } = await shell.run(
+    "./dist/bin.js -n typescript \"export a Next.js page called CounterPage which renders a header saying Hello World, and a counter which is incremented by a blue button labeled Press Me\""
   );
 
   t.snapshot(stdout);
