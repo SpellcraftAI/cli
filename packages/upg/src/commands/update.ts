ee6d7a9ef05b6883b849b68fb3727d6fd0d0ec48
C. Lewis <ctj.lewis@icloud.com>
2022-12-03 02:32:44 -0600

    chore: npm update fix

--- packages/upg/src/commands/update.ts
+++ packages/upg/src/commands/update.ts
@@ -50,7 +50,7 @@ export const updateCommand = async () => {
     }
 
     log("Updating upg via NPM.", ["bold"]);
-    await shell.run(`npm update -g ${packageName}`);
+    await shell.run(`npm i -g ${packageName}`);
   }
 
   success("Updated successfully.");
